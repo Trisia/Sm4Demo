ce573959827b673d98b3d07710986e0e1dee2ee6
cliven <quanguanyu@qq.com>
2020-02-18 16:03:22 +0800

    增加了IV

--- Sm4Demo/Program.cs
+++ Sm4Demo/Program.cs
@@ -13,19 +13,21 @@ namespace Sm4Demo
         { 
             byte[] plaintext = Encoding.ASCII.GetBytes("Hello World");
             byte[] keyBytes = Encoding.ASCII.GetBytes("0123456789ABCDEF");
-            byte[] iv = new byte[16];
-
+            byte[] iv = Encoding.ASCII.GetBytes("0123456789ABCDEF");
             // 加密
             KeyParameter key = ParameterUtilities.CreateKeyParameter("SM4", keyBytes);
+            ParametersWithIV keyParamWithIv = new ParametersWithIV(key, iv);
+
             IBufferedCipher inCipher = CipherUtilities.GetCipher("SM4/CBC/PKCS7Padding");
-            inCipher.Init(true, key);
+            inCipher.Init(true, keyParamWithIv);
+
             inCipher.ProcessBytes(plaintext);
             byte[] cipher = inCipher.DoFinal();
             Console.WriteLine("加密后的密文(hex): {0}", BitConverter.ToString(cipher, 0).Replace("-", string.Empty));
 
             // 解密
             inCipher.Reset();
-            inCipher.Init(false, key);
+            inCipher.Init(false, keyParamWithIv);
             inCipher.ProcessBytes(cipher);
             byte[] bin = inCipher.DoFinal();
             string ans = Encoding.UTF8.GetString(bin);
