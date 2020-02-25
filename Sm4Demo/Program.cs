d425c1b8ffeb3d1c22ebbdc480ee75873211c4a9
cliven <quanguanyu@qq.com>
2020-02-25 10:57:14 +0800

    由于分组的存在直接调用 DoFinal就可以不需要手动分组

--- Sm4Demo/Program.cs
+++ Sm4Demo/Program.cs
@@ -20,16 +20,13 @@ namespace Sm4Demo
 
             IBufferedCipher inCipher = CipherUtilities.GetCipher("SM4/CBC/PKCS7Padding");
             inCipher.Init(true, keyParamWithIv);
-
-            inCipher.ProcessBytes(plaintext);
-            byte[] cipher = inCipher.DoFinal();
+            byte[] cipher = inCipher.DoFinal(plaintext);
             Console.WriteLine("加密后的密文(hex): {0}", BitConverter.ToString(cipher, 0).Replace("-", string.Empty));
 
             // 解密
             inCipher.Reset();
             inCipher.Init(false, keyParamWithIv);
-            inCipher.ProcessBytes(cipher);
-            byte[] bin = inCipher.DoFinal();
+            byte[] bin = inCipher.DoFinal(cipher);
             string ans = Encoding.UTF8.GetString(bin);
 
             Console.WriteLine("解密明文内容:  {0}\t是否匹配: {1}", ans, Enumerable.SequenceEqual(plaintext, bin));
